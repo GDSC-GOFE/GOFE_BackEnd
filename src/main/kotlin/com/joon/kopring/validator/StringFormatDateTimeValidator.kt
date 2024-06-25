d07c8c03d01fea56d9e8b1939780154897b6d656
JoonYong <joonyongpark99@gmail.com>
2024-06-25 23:46:08 +0900

    GOFE-015 [STYLE] 스타일 변경 - 구글 자바 스타일 가이드 적용

--- src/main/kotlin/com/joon/kopring/validator/StringFormatDateTimeValidator.kt
+++ src/main/kotlin/com/joon/kopring/validator/StringFormatDateTimeValidator.kt
@@ -6,46 +6,26 @@ import jakarta.validation.ConstraintValidatorContext
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-class StringFormatDateTimeValidator : ConstraintValidator<StringFormatDateTime, String>{
+class StringFormatDateTimeValidator : ConstraintValidator<StringFormatDateTime, String> {
 
 
-
-    private var pattern : String?=null
+    private var pattern: String? = null
 
     override fun initialize(constraintAnnotation: StringFormatDateTime?) {
         this.pattern = constraintAnnotation?.pattern
     }
 
 
-
-
-
     // 정상이면 true, 비정상이면 false
     override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean {
-        return try{
+        return try {
             LocalDateTime.parse(value, DateTimeFormatter.ofPattern(pattern))
             true
-        }catch (e:Exception){
+        } catch (e: Exception) {
             false
 
 
-
-
-
-
-
-
-
 // 하하
-
-
-
-
-
-
-
-
-
 
 
         }
